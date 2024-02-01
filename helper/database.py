4a0c245e38568f8e8905a2679a6eeab6636369c5
python israel <104791322+nhman-python@users.noreply.github.com>
2024-02-01 03:06:49 +0200

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -11,10 +11,12 @@ class GroupInfo(Base):
     id = Column(Integer, primary_key=True, index=True)
     chat_id = Column(Integer, index=True)
     ignore_id = Column(Integer)
+    linked_channel = Column(Integer, nullable=True)
 
-    def __init__(self, chat_id, ignore_id):
+    def __init__(self, chat_id, ignore_id, linked_channel=None):
         self.chat_id = chat_id
         self.ignore_id = ignore_id
+        self.linked_channel = linked_channel
 
 
 Base.metadata.create_all(engine)
@@ -26,11 +28,11 @@ def get_all_ignore_ids(chat_id: int):
         return [c_id.ignore_id for c_id in result]
 
 
-def create_new_ignore_id(chat_id: int, ignore_id: int):
+def create_new_ignore_id(chat_id: int, ignore_id: int, linked_channel: int = None):
     with Session() as session:
         if session.query(GroupInfo).filter_by(chat_id=chat_id, ignore_id=ignore_id).first():
             return False
-        ignore = GroupInfo(chat_id=chat_id, ignore_id=ignore_id)
+        ignore = GroupInfo(chat_id=chat_id, ignore_id=ignore_id, linked_channel=linked_channel)
         session.add(ignore)
         session.commit()
         return True
